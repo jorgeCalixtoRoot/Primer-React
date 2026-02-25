cd991d053fec3941f7e779761615d678a303a600
Jorge Calixto Gonzalez <jorgecalixtogithub@gmail.com>
2026-02-25 19:10:46 +0100

    Hola mundo

--- src/App.tsx
+++ src/App.tsx
@@ -14,8 +14,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <h2>Hola Mundo</h2>
+      <h1>Hola Mundo</h1>
     </>
   )
 }
